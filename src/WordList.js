db0c9e9c8d838b22488e02ace4abcc6cddfd98d8
Alexandre Lagrange <alexandre.lagrange@eniro.com>
2018-10-13 18:19:43 +0200

    add bigger smaller words

--- src/WordList.js
+++ src/WordList.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
 
 class WordList extends Component {
-
-    displayWords(item) {
-        console.log(item);
-        return <li 
-            key={item[0]} 
-            className={"weight"+item[1]}>
-            {item[0]}
-        </li>
+  
+  constructor(props) {
+    super(props);
+    this.decrease = this.decrease.bind(this);
+    this.increase = this.increase.bind(this);
+  }
+  
+  displayWords(item, number) {
+    return <li 
+    key={item} 
+    className={"weight"+number}
+    id={item}>
+    {item}, {number} 
+    <input className={item} type="button" onClick={this.decrease} value="-"></input>
+    <input className={item} type="button" onClick={this.increase} value="+"></input>
+    </li>
+  }
+  
+  displayList() {
+    const list = [];
+    for (var [key, value] of this.props.items) {
+      list.push(this.displayWords(key, value))
     }
-
-    render() {
-      return (
-        <ul>
-          {[...this.props.items].map((item) => (
-              this.displayWords(item)
-          ))}
-        </ul>
+    return list;
+  }
+  
+  decrease(key) {
+    const keyToAddress = key.target.className
+    this.setState({
+      lol:"lol"
+    })
+    this.props.items.set(keyToAddress, this.props.items.get(keyToAddress)-1)
+  }
+  
+  increase(key) {
+    this.setState({
+      lol:"lol"
+    })
+    const keyToAddress = key.target.className
+    this.props.items.set(keyToAddress, this.props.items.get(keyToAddress)+1)
+  }
+  
+  render() {
+    return (
+      <ul>
+      {this.displayList()}
+      </ul>
       );
     }
   }
-
+  
   export default WordList;
